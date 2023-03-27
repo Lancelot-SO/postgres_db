5d4e5656043d71fc166b43ad4417b71c6ed13c5c
Lancelot-SO <fsowah001@gmail.com>
2023-03-27 17:09:54 +0000

    add queries.sql and data.sql

--- data.sql
+++ data.sql
@@ -1,2 +1,7 @@
 /* Populate database with sample data. */
 
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) Values (1, 'Agumon', '2020-02-03', 0, true, 10.23);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) Values (2, 'Gabumon', '2018-11-15', 2, true, 8);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) Values (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) Values (4, 'Devimon', '2018-05-17', 5, true, 11);
+SELECT * FROM animals;
