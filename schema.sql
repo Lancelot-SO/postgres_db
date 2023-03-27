f930a21d6137cfbd614c29b002c7e9544bda68af
Lancelot-SO <fsowah001@gmail.com>
2023-03-27 17:05:30 +0000

    create vet_clinic database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DECIMAL(5, 2) NOT NULL,
+  PRIMARY KEY (id)
 );
+SELECT * FROM animals;
+
+
